20b6bac3a5620c37bfd4661f01712dd59ca5fa6a
John Oliver <johnoliver616@yahoo.com>
2025-09-16 07:44:43 -0500

    week 2 scores

--- src/pages/matchups_and_spreads_page.py
+++ src/pages/matchups_and_spreads_page.py
@@ -18,7 +18,7 @@ def matchups_and_spreads_page(app_config: dict):
 
     with mid:
         st.title(f"Matchups and Spreads")
-        week_choice = st.selectbox("Select Week", weeks, index=1)
+        week_choice = st.selectbox("Select Week", weeks, index=2)
         week = int(week_choice[-1])
 
     # load schedule
